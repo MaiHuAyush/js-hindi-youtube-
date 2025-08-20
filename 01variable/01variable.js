c75ba22bf49030bc081044e5a012f9102d79ee48
Ayush Gupta <103426507+MaiHuAyush@users.noreply.github.com>
2025-08-20 15:19:13 +0000

    done data types

---
+++ 01variable/01variable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "ayushposo343@gmail.com"
+var accountPassword = "12345"
+accountCity = "Howrah"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "madedits95@madedits.com"
+accountPassword = "21211212"
+accountCity = "Kolkata"
+/*
+prefer not to use var beacause of black scope issue
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 02_Data Types/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // alert will only work in broswer
+
+console.log(3+3)
+
+console.log("Ayush");
+
+let name = "Ayush"
+let age = 13
+let isLoggedIn = false
+
+console.log(typeof "Ayush");
+
+
